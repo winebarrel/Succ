2d66d3489dafb83855b5f13e7308d37fbe6ddc03
winebarrel <sugawara@winebarrel.jp>
2024-02-21 10:08:34 +0900

    first commit

---
+++ Succ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Succ
+//
+//  Created by 菅原元気 on 2024/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Succ/SuccApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuccApp.swift
+//  Succ
+//
+//  Created by 菅原元気 on 2024/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SuccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
