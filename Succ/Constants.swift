5c540b0906552e7f8eefdf5233d9298402e0d2ac
winebarrel <sugawara@winebarrel.jp>
2024-02-22 15:58:16 +0900

    Fix default query

--- Succ/Constants.swift
+++ Succ/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 enum Constants {
-    static let defaultGithubQuery = "is:open is:pr author:@me archived:false"
+    static let defaultGithubQuery = "is:open is:pr author:@me draft:false archived:false"
     static let defaultTimerInterval: TimeInterval = 60
 }
