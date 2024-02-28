a3aff41e499fa90d6c89284f7f01d99d11c11a4a
winebarrel <sugawara@winebarrel.jp>
2024-02-28 12:11:34 +0900

    Update defaultTimerInterval

--- Succ/Constants.swift
+++ Succ/Constants.swift
@@ -2,5 +2,5 @@ import Foundation
 
 enum Constants {
     static let defaultGithubQuery = "is:open is:pr author:@me draft:false archived:false"
-    static let defaultTimerInterval: TimeInterval = 60
+    static let defaultTimerInterval: TimeInterval = 30
 }
